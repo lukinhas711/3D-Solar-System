2b3aa0eb45bf14f570d64f1e1c0a8334b0349ac6
Lucas <lucas.ribeiro711@gmail.com>
2023-06-03 14:30:10 -0300

    first animation

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# 3D-Solar-System
+# 3D Solar System
+
+This project is a 3D visualization of the solar system using WebGL and Three.js.
+
+## Getting Started
+
+Follow these steps to set up and run the project:
+
+## Prerequisites
+
+Make sure you have Node.js installed on your system.
+
+## Installation
+
+To install the dependencies and run the project, execute the following command in your terminal:
+
+```npm i && npx vite```
+This command will install the required dependencies and start the Vite development server.
+
+## Usage
+
+Open your browser and navigate to <http://localhost:3000> (or the port indicated by the Vite server) to view the 3D solar system visualization.
+
+Feel free to customize this documentation according to your project's needs. If you have any questions or need help with the documentation, feel free to ask!
--- index.js
+++ index.js
@@ -1,15 +1,58 @@
+import * as THREE from "three";
+
 class SistemaSolar {
   constructor() {
-      this.canvas = document.getElementById('sistema-solar');
-      this.ctx = this.ctx = this.canvas.getContext('webgl') || this.canvas.getContext('experimental-webgl');
-      this.resizeCanvas();
-      window.addEventListener('resize', () => this.resizeCanvas());
+    // Obtenha o elemento canvas
+    this.canvas = document.getElementById('sistema-solar');
+
+    // Crie a cena
+    this.scene = new THREE.Scene();
+
+    // Crie a câmera
+    this.camera = new THREE.PerspectiveCamera(
+        75,
+        this.canvas.clientWidth / this.canvas.clientHeight,
+        0.1,
+        1000
+    );
+
+    // Crie o renderizador e configure-o para usar o canvas
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+    // Crie um cubo
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
+
+    this.camera.position.z = 5;
+
+    // Redimensione o canvas
+    window.addEventListener('resize', () => this.resizeCanvas());
+
+    // Inicie a animação
+    this.animate();
+}
+
+  animate() {
+    requestAnimationFrame(() => this.animate());
+
+    // Rotacione o cubo
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    // Renderize a cena
+    this.renderer.render(this.scene, this.camera);
   }
 
   resizeCanvas() {
-      this.canvas.width = window.innerWidth;
-      this.canvas.height = window.innerHeight;
-  }
+    this.canvas.width = this.canvas.clientWidth;
+    this.canvas.height = this.canvas.clientHeight;
+    this.camera.aspect = this.canvas.clientWidth / this.canvas.clientHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+}
 }
 
-window.addEventListener('DOMContentLoaded', () => new SistemaSolar());
+window.addEventListener("DOMContentLoaded", () => new SistemaSolar());
