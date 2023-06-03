73fc46d9cb84f2ab5f347ff2d19c855f3a821e4b
Lucas <lucas.ribeiro711@gmail.com>
2023-06-03 15:39:01 -0300

    add sun

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import * as THREE from "three";
 class SistemaSolar {
   constructor() {
     this.canvas = document.getElementById('sistema-solar');
-    // cria a cena
+
     this.scene = new THREE.Scene();
 
-    // cria a camera
     this.camera = new THREE.PerspectiveCamera(
         75,
         this.canvas.clientWidth / this.canvas.clientHeight,
@@ -14,33 +13,33 @@ class SistemaSolar {
         1000
     );
 
-    // Cria o renderizador e configure-o para usar o canvas
     this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
-    // Cria um cubo
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.load('./textures/2k_sun.jpg', (texture) => {
+
+      const geometry = new THREE.SphereGeometry(2, 32, 32);
+      const material = new THREE.MeshBasicMaterial({ map: texture });
+      this.sun = new THREE.Mesh(geometry, material);
+      this.scene.add(this.sun);
+
+      this.camera.position.set(0, 3, 5);
+      this.camera.lookAt(new THREE.Vector3(0, 1, 0));
+
+      this.animate();
+    });
 
     this.camera.position.z = 5;
 
-    // Redimensiona o canvas
     window.addEventListener('resize', () => this.resizeCanvas());
-
-    // Inicia a animação
-    this.animate();
-}
+  }
 
   animate() {
     requestAnimationFrame(() => this.animate());
 
-    // Rotaciona o cubo
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    this.sun.rotation.y += 0.01;
 
-    // Renderiza a cena
     this.renderer.render(this.scene, this.camera);
   }
 
@@ -50,7 +49,7 @@ class SistemaSolar {
     this.camera.aspect = this.canvas.clientWidth / this.canvas.clientHeight;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
-}
+  }
 }
 
 window.addEventListener("DOMContentLoaded", () => new SistemaSolar());
